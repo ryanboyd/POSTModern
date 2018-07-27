b070ba0a520f452a67f5695d76f39d7079f9c5e9
Ryan L. Boyd <ahitpointhero@gmail.com>
2018-07-27 18:33:08 -0400

    v0.971
    Disabled the "prefer 32-bit" option. This should help skirt memory errors that can occur from long run-on texts.

--- WindowsFormsApplication1/Splash.Designer.cs
+++ WindowsFormsApplication1/Splash.Designer.cs
@@ -56,12 +56,12 @@
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(84, 288);
+            this.label1.Location = new System.Drawing.Point(79, 288);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(232, 33);
+            this.label1.Size = new System.Drawing.Size(246, 33);
             this.label1.TabIndex = 1;
-            this.label1.Text = "POSTModern v0.97";
+            this.label1.Text = "POSTModern v0.971";
             this.label1.UseWaitCursor = true;
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
